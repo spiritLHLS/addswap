d06ffb653090367db69b7c49c2ed74394d61b235
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2022-08-14 20:19:41 +0800

    Update ovzswap.sh

--- ovzswap.sh
+++ ovzswap.sh
@@ -1,44 +1,89 @@
 #!/bin/bash
 #频道：https://t.me/vps_reviews
 
-Green="\033[32m"
-Font="\033[0m"
-Red="\033[31m" 
+# 自定义字体彩色，read 函数，安装依赖函数
+red(){ echo -e "\033[31m\033[01m$@\033[0m"; }
+green(){ echo -e "\033[32m\033[01m$@\033[0m"; }
+yellow(){ echo -e "\033[33m\033[01m$@\033[0m"; }
+reading(){ read -rp "$(green "$1")" "$2"; }
 
-#root权限
-root_need(){
-    if [[ $EUID -ne 0 ]]; then
-        echo -e "${Red}Error:This script must be run as root!${Font}"
-        exit 1
-    fi
+# 必须以root运行脚本
+check_root(){
+  [[ $(id -u) != 0 ]] && red " The script must be run as root, you can enter sudo -i and then download and run again." && exit 1
+}
+
+#检查架构
+check_virt(){
+  virtcheck=$(systemd-detect-virt)
+  case "$virtcheck" in
+    kvm ) VIRT='kvm';;
+    openvz ) VIRT='openvz';;
+    * ) VIRT='kvm';;
+  esac
 }
 
 add_swap(){
 echo -e "${Green}请输入需要添加的swap，建议为内存的2倍！${Font}"
 read -p "请输入swap数值:" SWAP
 echo 'Start adding SWAP space ......';
-# SWAP="${1:-512}";
-NEW="$[SWAP*1024]";
-TEMP="${NEW//?/ }";
-OLD="${TEMP:1}0";
-umount /proc/meminfo 2> /dev/null
-sed "/^Swap\(Total\|Free\):/s,$OLD,$NEW," /proc/meminfo > /etc/fake_meminfo
-mount --bind /etc/fake_meminfo /proc/meminfo
-echo -e "${Green}swap创建成功，并查看信息：${Font}"
-free -m
+if [ $VIRT = 'openvz' ]; then
+    NEW="$[SWAP*1024]";
+    TEMP="${NEW//?/ }";
+    OLD="${TEMP:1}0";
+    umount /proc/meminfo 2> /dev/null
+    sed "/^Swap\(Total\|Free\):/s,$OLD,$NEW," /proc/meminfo > /etc/fake_meminfo
+    mount --bind /etc/fake_meminfo /proc/meminfo
+    echo -e "${Green}swap创建成功，并查看信息：${Font}"
+    free -m
+else
+    #检查是否存在swapfile
+    grep -q "swapfile" /etc/fstab
+    
+    #如果不存在将为其创建swap
+    if [ $? -ne 0 ]; then
+        echo -e "${Green}swapfile未发现，正在为其创建swapfile${Font}"
+        fallocate -l ${swapsize}M /swapfile
+        chmod 600 /swapfile
+        mkswap /swapfile
+        swapon /swapfile
+        echo '/swapfile none swap defaults 0 0' >> /etc/fstab
+             echo -e "${Green}swap创建成功，并查看信息：${Font}"
+             cat /proc/swaps
+             cat /proc/meminfo | grep Swap
+    else
+        echo -e "${Red}swapfile已存在，swap设置失败，请先运行脚本删除swap后重新设置！${Font}"
+    fi
+fi
 }
 
 del_swap(){
-echo 'Start adding SWAP space ......';
-SWAP=0;
-NEW="$[SWAP*1024]";
-TEMP="${NEW//?/ }";
-OLD="${TEMP:1}0";
-umount /proc/meminfo 2> /dev/null
-sed "/^Swap\(Total\|Free\):/s,$OLD,$NEW," /proc/meminfo > /etc/fake_meminfo
-mount --bind /etc/fake_meminfo /proc/meminfo
-echo -e "${Green}swap删除成功，并查看信息：${Font}"
-free -m
+if [ $VIRT = 'openvz' ]; then
+    echo 'Start deleting SWAP space ......';
+    SWAP=0;
+    NEW="$[SWAP*1024]";
+    TEMP="${NEW//?/ }";
+    OLD="${TEMP:1}0";
+    umount /proc/meminfo 2> /dev/null
+    sed "/^Swap\(Total\|Free\):/s,$OLD,$NEW," /proc/meminfo > /etc/fake_meminfo
+    mount --bind /etc/fake_meminfo /proc/meminfo
+    echo -e "${Green}swap删除成功，并查看信息：${Font}"
+    free -m
+else
+    #检查是否存在swapfile
+    grep -q "swapfile" /etc/fstab
+
+    #如果存在就将其移除
+    if [ $? -eq 0 ]; then
+        echo -e "${Green}swapfile已发现，正在将其移除...${Font}"
+        sed -i '/swapfile/d' /etc/fstab
+        echo "3" > /proc/sys/vm/drop_caches
+        swapoff -a
+        rm -f /swapfile
+        echo -e "${Green}swap已删除！${Font}"
+    else
+        echo -e "${Red}swapfile未发现，swap删除失败！${Font}"
+    fi
+fi
 }
 
 #开始菜单
