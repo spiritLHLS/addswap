1c0bc83e3ba003af089a4312b609537f55a74a74
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2022-08-14 19:56:19 +0800

    Update ovzswap.sh

--- ovzswap.sh
+++ ovzswap.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
-#Addition SWAP For OpenVZ
+#频道：https://t.me/vps_reviews
+
+Green="\033[32m"
+Font="\033[0m"
+Red="\033[31m" 
+
+#root权限
+root_need(){
+    if [[ $EUID -ne 0 ]]; then
+        echo -e "${Red}Error:This script must be run as root!${Font}"
+        exit 1
+    fi
+}
+
+add_swap(){
+echo -e "${Green}请输入需要添加的swap，建议为内存的2倍！${Font}"
+read -p "请输入swap数值:" SWAP
 echo 'Start adding SWAP space ......';
-SWAP="${1:-512}";
+# SWAP="${1:-512}";
 NEW="$[SWAP*1024]";
 TEMP="${NEW//?/ }";
 OLD="${TEMP:1}0";
@@ -9,3 +25,44 @@ umount /proc/meminfo 2> /dev/null
 sed "/^Swap\(Total\|Free\):/s,$OLD,$NEW," /proc/meminfo > /etc/fake_meminfo
 mount --bind /etc/fake_meminfo /proc/meminfo
 echo 'Add the ready!';
+}
+
+del_swap(){
+echo 'Start adding SWAP space ......';
+SWAP=0;
+NEW="$[SWAP*1024]";
+TEMP="${NEW//?/ }";
+OLD="${TEMP:1}0";
+umount /proc/meminfo 2> /dev/null
+sed "/^Swap\(Total\|Free\):/s,$OLD,$NEW," /proc/meminfo > /etc/fake_meminfo
+mount --bind /etc/fake_meminfo /proc/meminfo
+echo 'Add the ready!';
+}
+
+#开始菜单
+main(){
+root_need
+ovz_no
+clear
+echo -e "———————————————————————————————————————"
+echo -e "${Green}Linux VPS一键添加/删除swap脚本${Font}"
+echo -e "${Green}1、添加swap${Font}"
+echo -e "${Green}2、删除swap${Font}"
+echo -e "———————————————————————————————————————"
+read -p "请输入数字 [1-2]:" num
+case "$num" in
+    1)
+    add_swap
+    ;;
+    2)
+    del_swap
+    ;;
+    *)
+    clear
+    echo -e "${Green}请输入正确数字 [1-2]${Font}"
+    sleep 2s
+    main
+    ;;
+    esac
+}
+main
